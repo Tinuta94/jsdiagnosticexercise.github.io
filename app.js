4dd0fd7d6166bebd7b6c32ccebb526170bb26e8e
Tinuta94 <cristina.crestenciuc@gmail.com>
2019-08-05 16:17:22 -0400

    withdraw

--- app.js
+++ app.js
@@ -9,5 +9,10 @@
        document.getElementsByClassName('checkbalance')[0].innerHTML = checkBalance
     });
     
+       document.getElementById("takem").addEventListener("click", function(event) {
+        checkBalance -= parseInt(document.querySelector('#newinput').value);
+        console.log(checkBalance)
+        document.getElementsByClassName('checkbalance')[0].innerHTML = checkBalance
+     }); 
        
       
