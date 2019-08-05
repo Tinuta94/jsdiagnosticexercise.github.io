bf7ca6726c181854630442e484de0ff347ebb89d
Tinuta94 <cristina.crestenciuc@gmail.com>
2019-08-05 13:12:35 -0400

    show the first 2 results

---
+++ app.js
@@ -0,0 +1,6 @@
+//console.log("Hello from app.js")
+ 
+
+    document.getElementById("checkdep").addEventListener("click", function() {
+        console.log(document.getElementsByClassName("input")[0].value)
+    });
