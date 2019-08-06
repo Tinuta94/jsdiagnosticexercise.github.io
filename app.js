306a48506c69d525118fc2ecc0ecfee32d214aae
Tinuta94 <cristina.crestenciuc@gmail.com>
2019-08-06 07:44:06 -0400

    show final

--- app.js
+++ app.js
@@ -1,18 +1,34 @@
 //console.log("Hello from app.js")
 
  let checkBalance = 0;
-
-    document.getElementById("checkdep").addEventListener("click", function(event) {
-
-        checkBalance += parseInt(document.querySelector('#newinput').value);
+       document.getElementById("checkdep").addEventListener("click", function(event) {
+       checkBalance += parseInt(document.querySelector('#newinput').value);
        console.log(checkBalance)
        document.getElementsByClassName('checkbalance')[0].innerHTML = checkBalance
-    });
-    
-       document.getElementById("takem").addEventListener("click", function(event) {
+    if (checkBalance === 0) {
+        document.getElementById('checking').style.backgroundColor = 'red';
+    } 
+});
+        document.getElementById("takem").addEventListener("click", function(event) {
         checkBalance -= parseInt(document.querySelector('#newinput').value);
         console.log(checkBalance)
         document.getElementsByClassName('checkbalance')[0].innerHTML = checkBalance
-     }); 
+        if (checkBalance === 0) {
+            document.getElementById('checking').style.backgroundColor = 'red';
+        } 
+    }); 
        
+   let saveBalance = 0
+         document.getElementById("savedep").addEventListener("click", function(event) {
+         saveBalance += parseInt(document.querySelector('#newin').value);
+         console.log(saveBalance)
+         document.getElementsByClassName('savebalance')[0].innerHTML = saveBalance
+     });
+     
+         document.getElementById("takeout").addEventListener("click", function(event) {
+         saveBalance -= parseInt(document.querySelector('#newin').value);
+         console.log(saveBalance)
+         document.getElementsByClassName('savebalance')[0].innerHTML = saveBalance
+      }); 
+
       
